b5c89eafaa152fbe11e60b20de7287667b8be9ad
VerryWardana <118161469+VerryWardana@users.noreply.github.com>
2023-06-19 05:37:39 +0700

    Add files via upload

---
+++ rembangan part 5/rembangan part 5/Form3.Designer.cs
@@ -0,0 +1,53 @@
+﻿namespace rembangan_part_5
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            SuspendLayout();
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Center;
+            ClientSize = new Size(659, 138);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form3";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form3";
+            TopMost = true;
+            Load += Form3_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
